f614c11075864c434147e691e73afeefb23e9c62
Vijays2021 <90272892+Vijays2021@users.noreply.github.com>
2021-10-05 16:57:15 +0530

    Add files via upload

---
+++ employlist.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector : 'list-employ',
+  templateUrl : './employ.component.html',
+  styleUrls : ['./employ.component.css']
+})
+
+
+
+export class employlistComponent{
+    employees: any[] =[
+        //{ code: 'emp101', name: 'tom', gender: 'male', Salary: 2000},
+        
+        //{ code: 'emp102', name: 'abc', gender: 'female', Salary: 3000},
+        
+        //{ code: 'emp103', name: 'def', gender: 'male', Salary: 4000},
+        
+        //{ code: 'emp104', name: 'xyz', gender: 'female', Salary: 5000},
+        ];
+}
